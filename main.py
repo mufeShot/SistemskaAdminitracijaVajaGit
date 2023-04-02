cf818b5d6f6384e80e1a277f333b042dc2b028db
Aljaž <aljazfruhvirt@gmail.com>
2023-04-02 21:29:07 +0200

    Testiranje funkcije my_prewitt na slikah

--- main.py
+++ main.py
@@ -110,17 +110,18 @@ def main():
     # convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # testiranje my_prewitt
     cv2.imshow('Originalna slika', img)
-    roberts_edges = my_roberts(gray)
-    cv2.imshow('Roberts1', roberts_edges)
-    slika2 = spremeni_kontrast(gray, -3, 2)
-    cv2.imshow('Roberts2', my_roberts(slika2))
-    slika3 = spremeni_kontrast(gray, 2, 10)
-    cv2.imshow('Roberts3', my_roberts(slika3))
-    slika4 = spremeni_kontrast(gray, 1, 40)
-    cv2.imshow('Roberts4', my_roberts(slika4))
+    prewitt_edges = my_prewitt(gray)
+    cv2.imshow('Prewitt1', prewitt_edges)
+    slika2 = spremeni_kontrast(gray, 1.2, 2)
+    cv2.imshow('Prewitt2', my_prewitt(slika2))
+    slika3 = spremeni_kontrast(gray, 1.7, 12)
+    cv2.imshow('Prewitt3', my_prewitt(slika3))
+    slika4 = spremeni_kontrast(gray, 0.45, 35)
+    cv2.imshow('Prewitt4', my_prewitt(slika4))
     slika5 = spremeni_kontrast(gray, 4, 75)
-    cv2.imshow('Roberts5', my_roberts(slika5))
+    cv2.imshow('Prewitt5', my_prewitt(slika5))
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
