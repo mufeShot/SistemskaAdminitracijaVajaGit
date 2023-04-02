2c486cc5d2157a4a1d5b1af1293126d852e8f161
Aljaž <aljazfruhvirt@gmail.com>
2023-04-02 21:25:45 +0200

    Implementacija funkcije my_roberts

--- main.py
+++ main.py
@@ -2,9 +2,27 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-def my_roberts(slika):
-    #vaša implementacija
-    return slika_robov
+def my_roberts(image):
+    kernel_x = np.array([[0, 0, 0], [0, 1, 0], [0, 0, -1]])
+    kernel_y = np.array([[0, 0, 0], [0, 0, 1], [0, -1, 0]])
+
+    # Pad the image with zeros to handle edges
+    padded_image = np.pad(image, ((1, 1), (1, 1)), mode='edge')
+
+    # Compute gradients in x and y directions
+    grad_x = convolve(padded_image, kernel_x)[1:-1, 1:-1]
+    grad_y = convolve(padded_image, kernel_y)[1:-1, 1:-1]
+
+    # Compute magnitude of gradient
+    gradient_mag = np.sqrt(grad_x ** 2 + grad_y ** 2)
+
+    # Normalize gradient magnitude
+    gradient_mag = (gradient_mag - np.min(gradient_mag)) / (np.max(gradient_mag) - np.min(gradient_mag)) * 255
+
+    # Convert gradient magnitude to uint8
+    gradient_mag = gradient_mag.astype(np.uint8)
+
+    return gradient_mag
 
 def my_prewitt(slika):
     #vaša implementacija
@@ -16,7 +34,7 @@ def my_sobel(slika):
 
 def canny(slika, sp_prag, zg_prag):
     #vaša implementacija
-    return slika_robov 
+    return slika_robov
 
 def main():
     # read image
